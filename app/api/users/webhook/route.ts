9063a955c905105a76e98f3a80d1ae07b919d5bf
Leandro Rodrigues <lj251004@gmail.com>
2025-02-19 18:06:18 -0300

    feat: set up database connection with drizzle and webhook handling for users synchronization

---
+++ app/api/users/webhook/route.ts
@@ -0,0 +1,78 @@
+import { headers } from "next/headers";
+
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { Webhook } from "svix";
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error("SIGNING_SECRET is not defined");
+  }
+
+  const wh = new Webhook(SIGNING_SECRET);
+
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Missing headers", { status: 400 });
+  }
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  let evt: WebhookEvent;
+
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (error) {
+    console.error("Failed to verify webhook: ", error);
+    return new Response("Failed to verify webhook", { status: 400 });
+  }
+
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    const { data } = evt;
+
+    await db.insert(users).values({
+      clerkId: data.id,
+      name: `${data.first_name} ${data.last_name}`,
+      imageUrl: data.image_url,
+    });
+  }
+
+  if (eventType === "user.deleted") {
+    const { data } = evt;
+
+    if (!data.id) return new Response("Missing user id", { status: 400 });
+
+    await db.delete(users).where(eq(users.clerkId, data.id));
+  }
+
+  if (eventType === "user.updated") {
+    const { data } = evt;
+
+    if (!data.id) return new Response("Missing user id", { status: 400 });
+
+    await db
+      .update(users)
+      .set({
+        name: `${data.first_name} ${data.last_name}`,
+        imageUrl: data.image_url,
+      })
+      .where(eq(users.clerkId, data.id));
+  }
+
+  return new Response("Webhook received", { status: 200 });
+}
